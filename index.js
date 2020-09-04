f341c348fcd73321049588e4e5b3db2f8f5fa0df
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-04 14:49:34 +0530

    Add code for html file

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 const server = http.createServer((req, res) => {
-  
+  if (req.method == "GET") {
+
+    let urlPath = req.url.split("/");
+    
+    // HTML file
+    if (urlPath[1] == "html") {
+      const filePathHTML = path.join(__dirname, "/public/html/index.html");
+      console.log("strating rading file ");
+      fs.readFile(filePathHTML, (err, content) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(content);
+        }
+      });
+    }
+    
+  }
 });
 
 const PORT = process.env.PORT || 5000;
