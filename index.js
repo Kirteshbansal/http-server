03ce75fb5e05c9e5c574a62ab7252a422852b6c2
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-09-09 23:43:21 +0530

    Update http.createServer() module

--- index.js
+++ index.js
@@ -1,83 +1,11 @@
 const http = require("http");
-const path = require("path");
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
-let uuid = uuidv4();
+const response = require("./response");
+
 const server = http.createServer((req, res) => {
   if (req.method == "GET") {
-    let urlPath = req.url.split("/");
-
-    // HTML file
-    if (urlPath[1] == "html") {
-      const filePathHTML = path.join(__dirname, "/public/html/index.html");
-      fs.readFile(filePathHTML, (err, content) => {
-        if (err) {
-          console.error(err);
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(content);
-        }
-      });
-    }
-    // JSON file
-    else if (urlPath[1] == "json") {
-      const filePathJSON = path.join(__dirname, "/public/json/data.json");
-      fs.readFile(filePathJSON, (err, content) => {
-        if (err) {
-          console.error(err);
-        } else {
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(content);
-        }
-      });
-    }
-    // UUID
-    else if (urlPath[1] == "uuid") {
-      content = uuid;
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ uuid: content }));
-    }
-    // Status & Status code
-    else if (urlPath[1] == "status") {
-      switch (urlPath[2]) {
-        case "100":
-          var status = "Continue";
-          break;
-        case "200":
-          status = "OK";
-          break;
-        case "300":
-          status = "Multiple Choices";
-          break;
-        case "400":
-          status = "Bad Request";
-          break;
-        case "500":
-          status = "Internal Server Error";
-          break;
-      }
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({ Status: `${status}`, "Status Code": `${urlPath[2]}` })
-      );
-    }
-    //  Delay in status
-    else if (urlPath[1] == "delay") {
-      if (parseInt(urlPath[2]) === Number(urlPath[2])) {
-        setTimeout(() => {
-          return res.end(
-            `Responsing after ${urlPath[2] + " seconds"}` +
-              "\n\n" +
-              JSON.stringify({ Status: "OK", "Status Code": `${200}` })
-          );
-        }, parseInt(urlPath[2]) * 1000);
-      }else{
-        res.end(
-          `Responsing after ${0 + " seconds"}` +
-            "\n\n" +
-            JSON.stringify({ Status: "Bad Request", "Status Code": `${400}` }))
-      }
-    }
+    response.httpRequest(req,res);
+  }else{
+    res.end(`Invalid method '${req.method}'`)
   }
 });
 
